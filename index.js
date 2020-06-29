1f1655ee6c805c542511fe247dc3bd4a53be8a77
McBryan <solomonmcbryan@gmail.com>
2020-06-29 19:49:50 +0000

    Added Specific Route Middleware

--- index.js
+++ index.js
@@ -7,16 +7,31 @@ const handleAllRequest = (req, res) => {
   console.log("Someone requested for profile page");
   res.send("<h1>This is My profile Page</h1>");
 };
+const handleProfileRequest = (req, res) => {
+  console.log("Someone requested for profile page");
+  res.send("This is My profile Page");
+};
 
-//Adding Middlewares
-const middlewareFunction = () =>{
+
+//Adding Middleware 
+const middlewareFunction = (req, res, next) =>{
   console.log("middlewareFunction")
+  next();
 }
 
+//Adding Specific Middleware
+const profileMiddlewareFunction = (req, res, next) =>{
+  console.log("Specific profile middleware Function executed")
+  next();
+}
 
-//Server Methods
+//Server Methods (General Middleware First before any other route )
 server.use(middlewareFunction)
-server.get('/profile', handleAllRequest);
+server.get('/profile', profileMiddlewareFunction, handleAllRequest);
+server.post('/profile', handleProfileRequest);
+
+
+
 
 //Listening To Server
 server.listen(3000, () => console.log("Server Is Ready For Any Request"));
